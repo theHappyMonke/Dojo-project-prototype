b8d797bdace344e4904dec5772f864d2b682ba84
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-11-25 13:00:17 +0000

    correcting some issues

--- queries.sql
+++ queries.sql
@@ -26,6 +26,6 @@ INSERT INTO access_rights VALUES (NULL, 'organiser', 0);
 INSERT INTO access_rights VALUES (NULL, 'lead', 0);
 INSERT INTO access_rights VALUES (NULL, 'admin', 0);
 
-UPDATE users SET access = 4 WHERE id = 1
+UPDATE users SET access = 4 WHERE id = 1;
 UPDATE access_rights SET quantity = 0 WHERE id = 1;
 UPDATE access_rights SET quantity = 1 WHERE id = 4;
