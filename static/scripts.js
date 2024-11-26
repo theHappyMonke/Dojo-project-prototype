8afc0ac37e196e9b0252d754d7cefd11948f9a93
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-11-26 12:54:24 +0000

    correcting some issues

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,19 @@
-function resetForm(){document.getElementById("setup-session").reset()}
+function resetForm(){
+    document.getElementById("setup-session").reset()
+}
+
+function changeClass(name) {
+    var element = document.querySelector("#" + name);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+
+    var span = document.getElementById('expand' + name);
+    if (span.innerHTML === "Click box to expand") {
+        document.getElementById('expand' + name).innerHTML = "Click box to collapse";
+    } else {
+        document.getElementById('expand' + name).innerHTML = "Click box to expand";
+    }
+}
